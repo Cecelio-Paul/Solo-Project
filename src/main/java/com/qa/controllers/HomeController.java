1d23835f79b71a6f6d3e67d55789bd6647137353
Cecelio-Paul <david.paul@academytrainee.com>
2019-09-09 14:49:24 +0100

    Creating a controllers package under the com.qa package so it can be recognised. Also inserting @RestController and @RequestMapping to return Back-End Poker API.

---
+++ src/main/java/com/qa/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.qa.controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home(){
+
+        return "Back-End Poker API";
+
+    }
+
+}
