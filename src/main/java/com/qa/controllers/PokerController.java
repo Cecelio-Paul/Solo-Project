bb3e3f6c5bb661d9e10a7cc60892af8deeabf942
Cecelio-Paul <david.paul@academytrainee.com>
2019-09-09 15:31:29 +0100

    Finished the PokerController class in controllers package. It will add a new data to a list. It utilises GET and POST currently and I have commented out the functionality of DELETE.

--- src/main/java/com/qa/controllers/PokerController.java
+++ src/main/java/com/qa/controllers/PokerController.java
@@ -1,8 +1,13 @@
 package com.qa.controllers;
 
-import com.qa.models.Note;
+import com.qa.models.Data;
+import com.qa.repository.NoteRepository;
+import com.qa.repository.PokerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import sun.misc.Request;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin()
@@ -10,6 +15,28 @@ import org.springframework.web.bind.annotation.*;
 public class PokerController {
 
     @Autowired
-    private
+    private PokerRepository repository;
 
+    @RequestMapping(value = "Datas", method = RequestMethod.GET)
+    public List<Data> listAllNotes(){
+        return repository.findAll(); }
+
+    @RequestMapping(value = "Datas", method = RequestMethod.POST)
+    public Data addData(@RequestBody Data data){
+        return repository.saveAndFlush(data);
+    }
+
+    @RequestMapping(value = "Datas/{id}", method = RequestMethod.GET) //only the 'datas' of id specifically gets sent back.
+    public Data getData (@PathVariable Long id) {
+        return repository.findOne(id);
+    }
+
+//    @RequestMapping(value = "Datas/{id}", method = RequestMethod.DELETE)
+//    public Data deleteNote(@PathVariable Long id){
+//        Data existing = repository.findOne(id);
+//        repository.delete(existing);
+//        return existing;
+//  Used if you want to delete a piece of data corresponding to the id type.
+
+    }
 }
